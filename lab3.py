54657cf729d17d85f417b893cda25d0c3f25e538
Dmitriy B <ne_dumayte@mail.ru>
2023-12-05 12:14:07 +0300

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -2,19 +2,24 @@ from tkinter import *
 import random
 
 window = Tk()
+anime = 'wall.png'
 window.geometry('1400x1000')
-alfabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+image_ = PhotoImage(file=anime)
+bg_label = Label(window, image=image_)
+bg_label.place(x=0, y=0, relwidth=1, relheight=1)
 
 
 def generate_key(number):
 
+    alfabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     digits = [int(d) for d in str(number)]
 
     block_digits1 = random.sample(digits[3:], 3)
-    block3_1 = int(str(block_digits1[0]) + str(block_digits1[1]) + str(block_digits1[2]))
+    block3_1 = int(f"{block_digits1[0]}{block_digits1[1]}{block_digits1[2]}")
 
     block_digits2 = random.sample(digits[:3], 3)
-    block3_2 = int(str(block_digits2[0]) + str(block_digits2[1]) + str(block_digits2[2]))
+    block3_2 = int(f"{block_digits2[0]}{block_digits2[1]}{block_digits2[2]}")
 
     block_letters1 = random.sample(alfabet, 3)
 
@@ -29,13 +34,9 @@ def generate_key(number):
 def clicked():
     number = txt.get()
     key = generate_key(number)
-    lbl_key = Label(window, text='Ваш ключ: ' + key, font=("Arial Bold", 20))
+    lbl_key = Label(window, text=f'Ваш ключ: {key}', font=("Arial Bold", 20))
     lbl_key.grid(column=1, row=3, padx=30, pady=0)
 
-
-image_ = PhotoImage(file='wall.png')
-bg_label = Label(window, image=image_)
-bg_label.place(x=0, y=0, relwidth=1, relheight=1)
 
 lbl = Label(window, text='Введите 6-значное число:', font=("Times New Roman", 20))
 lbl.grid(column=1, row=0, padx=30, pady=30)
@@ -48,3 +49,4 @@ txt = Entry(window, width=3, font=("Times New Roman", 20))
 txt.focus()
 txt.grid(column=2, row=0, padx=0, pady=30)
 window.mainloop()
+
